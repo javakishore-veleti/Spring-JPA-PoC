7fbc69f44f9732556f836b203499eeb3868d27ba
Kishore Veleti <kishore.veleti@northadminsair2.home>
2020-05-06 01:48:26 -0400

    Implemented Spring JPA

---
+++ jk-spring-jpa-poc/src/test/java/jk/research/spring/jpa/common/cache/dao/CacheRefreshRegistrationDaoTest.java
@@ -0,0 +1,41 @@
+package jk.research.spring.jpa.common.cache.dao;
+
+import java.util.Date;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import jk.research.spring.jpa.common.cache.TargetApplication;
+import jk.research.spring.jpa.common.cache.TargetCache;
+import jk.research.spring.jpa.common.cache.model.CacheRefreshRegistration;
+import jk.research.spring.jpa.common.cache.model.CacheRefreshRegistrationId;
+
+@ContextConfiguration(locations = "classpath:application-context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CacheRefreshRegistrationDaoTest {
+
+	@BeforeClass
+	public static void setup() {
+	}
+
+	@Autowired
+	private CacheRefreshRegistrationDao cacheRefreshRegistrationDao;
+
+	@Test
+	public void saveTest() {
+
+		CacheRefreshRegistration cacheReg = null;
+		for (int aRowIndex = 1; aRowIndex <= 10; aRowIndex++) {
+
+			cacheReg = new CacheRefreshRegistration(
+					new CacheRefreshRegistrationId(TargetApplication.getRandom(), TargetCache.getRandom()), new Date());
+
+			cacheRefreshRegistrationDao.save(cacheReg);
+		}
+
+	}
+}
